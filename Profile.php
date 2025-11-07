1bbb3684decabb195516b0f24499973920a85a50
Lubis Karisma Ariansyah <72157953+arnshyz@users.noreply.github.com>
2025-11-07 22:13:56 +0700

    Delete Profile.php

--- Profile.php
+++
@@ -1,8 +0,0 @@
-<?php
-$appPageKey = 'profile';
-$appTitle = 'Profile – AI Hub + Filmmaker + UGC';
-$appInitialView = 'viewDashboard';
-$appAllowedViews = ['viewDashboard'];
-$appInitialFeature = '';
-require __DIR__ . '/includes/app_bootstrap.php';
-require __DIR__ . '/templates/app_layout.php';
