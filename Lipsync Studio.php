146bb1af8a37162d01b2687a897e5e186b94e252
Lubis Karisma Ariansyah <72157953+arnshyz@users.noreply.github.com>
2025-11-07 22:13:38 +0700

    Delete Lipsync Studio.php

--- Lipsync Studio.php
+++
@@ -1,8 +0,0 @@
-<?php
-$appPageKey = 'lipsync';
-$appTitle = 'Lipsync Studio – AI Hub + Filmmaker + UGC';
-$appInitialView = 'viewHub';
-$appAllowedViews = ['viewHub'];
-$appInitialFeature = 'lipsync';
-require __DIR__ . '/includes/app_bootstrap.php';
-require __DIR__ . '/templates/app_layout.php';
