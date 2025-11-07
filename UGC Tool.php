e62482aac979b4811efed7db548d0b7d7f7d5e1e
Lubis Karisma Ariansyah <72157953+arnshyz@users.noreply.github.com>
2025-11-07 22:14:21 +0700

    Delete UGC Tool.php

--- UGC Tool.php
+++
@@ -1,8 +0,0 @@
-<?php
-$appPageKey = 'ugc';
-$appTitle = 'UGC Tool – AI Hub + Filmmaker + UGC';
-$appInitialView = 'viewUGC';
-$appAllowedViews = ['viewUGC'];
-$appInitialFeature = '';
-require __DIR__ . '/includes/app_bootstrap.php';
-require __DIR__ . '/templates/app_layout.php';
