c4e8777b2d89ff8b9ed2d101c722347644b00c69
Lubis Karisma Ariansyah <72157953+arnshyz@users.noreply.github.com>
2025-11-07 22:14:04 +0700

    Delete Text Generator.php

--- Text Generator.php
+++
@@ -1,8 +0,0 @@
-<?php
-$appPageKey = 'textGen';
-$appTitle = 'Text Generator – AI Hub + Filmmaker + UGC';
-$appInitialView = 'viewHub';
-$appAllowedViews = ['viewHub'];
-$appInitialFeature = 'textGen';
-require __DIR__ . '/includes/app_bootstrap.php';
-require __DIR__ . '/templates/app_layout.php';
