02762502d5f40282ef7dd351729745b9734270b3
Lubis Karisma Ariansyah <72157953+arnshyz@users.noreply.github.com>
2025-11-07 22:14:28 +0700

    Delete Video Generator.php

--- Video Generator.php
+++
@@ -1,8 +0,0 @@
-<?php
-$appPageKey = 'videoGen';
-$appTitle = 'Video Generator – AI Hub + Filmmaker + UGC';
-$appInitialView = 'viewHub';
-$appAllowedViews = ['viewHub'];
-$appInitialFeature = 'videoGen';
-require __DIR__ . '/includes/app_bootstrap.php';
-require __DIR__ . '/templates/app_layout.php';
