682f2cc997c65331114183b993155704481fff15
Lubis Karisma Ariansyah <72157953+arnshyz@users.noreply.github.com>
2025-11-07 22:13:16 +0700

    Delete Filmmaker.php

--- Filmmaker.php
+++
@@ -1,8 +0,0 @@
-<?php
-$appPageKey = 'filmmaker';
-$appTitle = 'Filmmaker – AI Hub + Filmmaker + UGC';
-$appInitialView = 'viewFilm';
-$appAllowedViews = ['viewFilm'];
-$appInitialFeature = '';
-require __DIR__ . '/includes/app_bootstrap.php';
-require __DIR__ . '/templates/app_layout.php';
