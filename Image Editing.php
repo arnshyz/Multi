923715c17c28f2d82a9cb3c88d24a744758c736b
Lubis Karisma Ariansyah <72157953+arnshyz@users.noreply.github.com>
2025-11-07 22:13:23 +0700

    Delete Image Editing.php

--- Image Editing.php
+++
@@ -1,8 +0,0 @@
-<?php
-$appPageKey = 'imageEdit';
-$appTitle = 'Image Editing – AI Hub + Filmmaker + UGC';
-$appInitialView = 'viewHub';
-$appAllowedViews = ['viewHub'];
-$appInitialFeature = 'imageEdit';
-require __DIR__ . '/includes/app_bootstrap.php';
-require __DIR__ . '/templates/app_layout.php';
