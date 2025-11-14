fae85fc18fd00ad2290ca20e5c03309cf042b351
Lubis Karisma Ariansyah <72157953+arnshyz@users.noreply.github.com>
2025-11-14 08:25:32 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+// config.php berada di luar public_html
+// Ganti nilai API key di bawah ini atau gunakan environment variable FREEPIK_API_KEY di hPanel.
+return [
+    'FREEPIK_API_KEY' => 'FPSXbcc5610f664682840d2dfd832d74fc03',
+];
